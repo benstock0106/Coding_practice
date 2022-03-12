9b01f011dc1010746d8474f297fc821482ef4493
Ben <yurun0106@gmail.com>
2022-03-12 11:09:39 +0800

    guess_int1

--- import2.py
+++ import2.py
@@ -7,13 +7,17 @@
 import random
 
 r = random.randint(1,10)
+count = 0
 while True:
+    count += 1 #count = count + 1
     num = input('猜對有獎！請輸入數字： ')
     num = int(num)
     if r == num:
          print('終於猜對了')
+         print('這是你猜得第', count, '次')
          break
     elif num > r:
         print('猜錯了,再猜小一點')
     elif num < r:
         print('猜錯了,再猜大一點')
+    print('這是你猜得第', count, '次')
